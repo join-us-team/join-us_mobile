7003d439dcd416af641afe0874bc55f229e919e5
applejinn <7applejink@gmail.com>
2022-09-30 21:03:36 +0900

    page 컴포넌트 생성

---
+++ src/pages/JoinMatching.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function JoinMatching() {
+  return (
+    <div>
+      조인 매칭
+    </div>
+  );
+}
+
+export default JoinMatching;
---
+++ src/pages/MatchingStatus.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MatchingStatus() {
+  return (
+    <div>
+      매칭 현황
+    </div>
+  );
+}
+
+export default MatchingStatus;
