14f89a6dc99ef719f8d39d154b25e0495876c647
applejinn <7applejink@gmail.com>
2022-09-30 21:08:41 +0900

    Route 추가

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "../pages/Home";
+import JoinMatching from "../pages/JoinMatching";
+import MatchingStatus from "../pages/MatchingStatus";
+import SignUp from "../pages/SignUp";
 
 function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/" element={<JoinMatching />} />
+        <Route path="/" element={<MatchingStatus />} />
+        <Route path="/" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
