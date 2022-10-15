07877d86ea04370c959dcc4db827e6273404842b
applejinn <7applejink@gmail.com>
2022-10-15 15:28:17 +0900

    ADD Logo

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -3,14 +3,19 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   width: 640px;
-  height: 80px;
   border: 1px solid red;
+  padding: 10px 0 10px 10px;
+`
+const Logo = styled.div`
+  width: 100px;
+  height: 40px;
+  border: 1px solid purple;
 `
 
 function Header() {
   return (
     <Container>
-      헤더
+      <Logo />
     </Container>
   );
 }
